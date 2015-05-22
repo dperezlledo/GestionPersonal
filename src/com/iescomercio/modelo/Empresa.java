6325c4348e2136b257776d173f40df2a52735bb2
David Perez <dperez.comercio@gmail.com>
2015-05-22 21:21:13 +0200

    Mi primer commit sobre Gestion de personal

--- src/com/iescomercio/modelo/Empresa.java
+++ src/com/iescomercio/modelo/Empresa.java
@@ -6,10 +6,52 @@
 
 package com.iescomercio.modelo;
 
+import java.util.Collection;
+
 /**
  *
  * @author Vespertino
  */
 public class Empresa {
+    private String domicilio;
+    private String telefono;
+    private String nombre;
+    private Collection<Cliente> carteraClientes;
+    private Collection<Empleado> plantilla;
+
+    public Empresa() {
+        
+    }
     
+    public void setDatosEmpleado(String nombre, int edad, int sb) {
+        Empleado e = new Empleado();
+        e.setNombre(nombre);
+        e.setEdad(edad);
+        e.setSalarioBruto(sb);
+        if (!plantilla.contains(sb))
+            this.plantilla.add(sb);
+    }
+    
+    public Collection<Cliente> getCarteraClientes() {
+        return carteraClientes;
+    }
+    
+    public void addCliente(Cliente c) {
+        if (!carteraClientes.contains(c))
+            this.carteraClientes.add(c);
+    }
+    
+    public void removeCliente(Cliente c) {
+        if (carteraClientes.contains(c))
+            this.carteraClientes.remove(c);
+    }
+    
+    public void listaEmpleado() {       
+        
+        
+    }
+    
+    public void listaClientes() {
+        
+    }
 }
